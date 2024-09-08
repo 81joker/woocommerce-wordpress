72ea247ffca1ca35b6e4ba614cad6eb29f55ff5b
81joker <nehad.al.timimi@gmail.com>
2024-09-08 05:16:01 +0200

    add  Searching product

--- plugins/woocommerce/templates/archive-product.php
+++ plugins/woocommerce/templates/archive-product.php
@@ -27,10 +27,10 @@ get_header( 'shop' );
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
 do_action( 'woocommerce_before_main_content' );?>
-<header class="woocommerce-products-header bg-info">
-	<?php if(apply_filters('woocmmerce_show_page_title', true)): ?>
-		<h1 class="woocommerce-products-header_title page-title bg-light"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+<header class="woocommerce-products-header">
+	<?php /* if(apply_filters('woocmmerce_show_page_title', true)): */?>
+		<h1 class="woocommerce-products-header_title page-title bg-light"><?php /* woocommerce_page_title();*/ ?></h1>
+	<?php /* endif; */?>
 <?php
 /**
  * Hook: woocommerce_shop_loop_header.
